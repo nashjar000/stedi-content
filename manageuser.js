60bcc86f257d45a5d235b8ccb2f8dff631b7426b
Jared Nash <nash_jar000@byui.edu>
2023-01-27 08:41:06 -0700

    code from text message can almost be used in login

--- manageuser.js
+++ manageuser.js
@@ -11,10 +11,6 @@ function setusername(){
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
-    }
 }
 
 function setverifypassword(){
@@ -47,9 +43,10 @@ function checkexpiredtoken(token){
 }
 
 function oneTimePassword(){
+    const phoneNumber = document.getElementById('username').value
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/twofactorlogin/2089926343',
+        url: 'https://dev.stedi.me/twofactorlogin/'+ phoneNumber,
         data: JSON.stringify({userName, password}),
         contentType: "application/text",
         dataType: 'text'
@@ -64,8 +61,8 @@ function userlogin(){
     $.ajax({
         type: 'POST',
         // url: '/login',
-        url: 'https://dev.stedi.me/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber:'2089926343', oneTimePassword:'8271'}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
